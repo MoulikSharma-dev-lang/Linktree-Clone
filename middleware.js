81eb300209cbb2350ea652dafd5ab91c1ea89349
avinavsharma <avinavsharma007@gmail.com>
2024-12-11 19:23:44 +0530

    changes in middleware.js

--- middleware.js
+++ middleware.js
@@ -4,14 +4,14 @@ import { cookies } from "next/headers";
 export async function middleware(request){
     const cookieStore = await cookies()
     if (request.nextUrl.pathname.endsWith("/dashboard") || request.nextUrl.pathname.endsWith("/createbittree")){
-        const token = cookieStore.get("next-auth.session-token")
+        const token = cookieStore.get("__Secure-next-auth.session-token")
         if (!token){
             return NextResponse.redirect(`${process.env.NEXT_PUBLIC_HOST}/login`)
         } else {
             console.log("Everything is working fine!")
         }
     } else if (request.nextUrl.pathname.endsWith("/login")){
-        const token = cookieStore.get("next-auth.session-token")
+        const token = cookieStore.get("__Secure-next-auth.session-token")
         if (token){
             return NextResponse.redirect(`${process.env.NEXT_PUBLIC_HOST}/dashboard`)
         } else {
